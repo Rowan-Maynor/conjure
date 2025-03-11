630577a9776e176c3a036a3f32efa61bd79fec31
rowan-maynor <rowanmaynor@gmail.com>
2025-03-11 19:33:54 -0400

    Nav mech basics

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -4,25 +4,33 @@ var damage = 2
 var health = 10
 const speed = 100
 var current_command = "stop"
-var control = "player"
+@export var control = "player"
 
 var click_position = Vector2()
 var target_position = Vector2()
 var current_direction = "down"
+@onready var nav = $NavigationAgent2D
 
 func _ready():
 	click_position = position
+	if(control == "enemy"):
+		target_position.x = 864.0
+		target_position.y = 448.0
+		update_target_position(target_position)
 
 func _physics_process(_delta: float) -> void:
-	#if Input.is_action_just_pressed("stop_movement"):
-		#click_position = position
-		#current_command = "stop"
-	if position.distance_to(click_position) > 3:
+	if (position.distance_to(target_position) > 3 && control == "enemy"):
+		var next_nav_location = nav.get_next_path_position()
+		var nav_target_position = (next_nav_location - position).normalized()
+		velocity = nav_target_position * speed
+		handle_anim(nav_target_position)
+		move_and_slide()
+	if (position.distance_to(click_position) > 3 && control == "player"):
 		target_position = (click_position - position).normalized()
 		velocity = target_position * speed
 		handle_anim(target_position)
 		move_and_slide()
-	if (position.distance_to(click_position) < 3):
+	if (position.distance_to(click_position) < 3 && control == "player"):
 		$AnimatedSprite2D.play("idle_" + current_direction)
 		if(current_command == "move"):
 			current_command = "stop"
@@ -45,3 +53,5 @@ func handle_anim(vector):
 		$AnimatedSprite2D.play("move_up")
 		current_direction = "up"
 		
+func update_target_position(target):
+	nav.set_target_position(target)
