5652e346934ec54e6f6230f792e9341837e7c394
rowan-maynor <rowanmaynor@gmail.com>
2025-03-11 04:04:31 -0400

    Basics of drag selection are done

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,6 +7,12 @@ var t1_units = [
 	"res://scenes/units/t1/skeleton.tscn"
 ]
 
+#handles drag select
+var selected = []
+var drag_start = Vector2.ZERO
+@onready var selection_area = $selection_area
+@onready var selection_collision = $selection_area/CollisionShape2D
+
 func _ready():
 	load_player_data(player_data_path)
 	update_player_data_ui(player_data)
@@ -21,9 +27,10 @@ func _ready():
 	
 	print("Done!")
 
-func _input(_event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if(Input.is_action_pressed("clear_data")):
 		clear_player_data(player_data_path)
+		
 	if(Input.is_action_just_pressed("spawn_t1_unit")):
 		var spawn_areas = $player_spawn_areas.get_children()
 		var unit = load(t1_units.pick_random()).instantiate()
@@ -33,6 +40,75 @@ func _input(_event: InputEvent) -> void:
 		else:
 			unit.position = spawn_point.global_position
 			$".".add_child(unit)
+			
+	if(Input.is_action_just_released("right_click")):
+		for unit in selected:
+			unit.click_position = get_global_mouse_position()
+			unit.current_command = "move"
+			
+	if(Input.is_action_just_pressed("stop_movement")):
+		for unit in selected:
+			unit.click_position = unit.position
+			unit.current_command = "stop"
+			
+	if(drag_start == Vector2.ZERO && event is InputEventMouseButton 
+		&& event.button_index == 1 && event.is_pressed()):
+			selected = []
+			drag_start = get_global_mouse_position()
+	elif(drag_start != Vector2.ZERO && event is InputEventMouseButton 
+		&& event.button_index == 1):
+			_select_units()
+			drag_start = Vector2.ZERO
+
+func _process(_delta):
+	queue_redraw()
+
+func _draw():
+	if (drag_start == Vector2.ZERO):
+		return
+	var drag_end = get_global_mouse_position()
+	var start_x = drag_start.x
+	var start_y = drag_start.y
+	var end_x = drag_end.x
+	var end_y = drag_end.y
+	
+	var line_width = 3.0
+	var line_color = Color.WHITE
+	
+	draw_line(Vector2(start_x, start_y), Vector2(end_x, start_y), line_color, line_width)
+	draw_line(Vector2(start_x, start_y), Vector2(start_x, end_y), line_color, line_width)
+	draw_line(Vector2(end_x, start_y), Vector2(end_x, end_y), line_color, line_width)
+	draw_line(Vector2(start_x, end_y), Vector2(end_x, end_y), line_color, line_width)
+
+func _select_units():
+	var size = abs(get_global_mouse_position() - drag_start)
+	var area_position = _get_rect_start_position()
+	
+	selection_area.global_position = area_position
+	selection_collision.global_position = area_position + size / 2
+	selection_collision.shape.size = size
+	
+	await get_tree().create_timer(.04).timeout
+	
+	for body in selection_area.get_overlapping_bodies():
+		if (body.control == "player"):
+			selected.append(body)
+
+func _get_rect_start_position():
+	var new_position = Vector2.ZERO
+	var mouse_position = get_global_mouse_position()
+	
+	if (drag_start.x < mouse_position.x):
+		new_position.x = drag_start.x
+	else:
+		new_position.x = mouse_position.x
+	
+	if (drag_start.y < mouse_position.y):
+		new_position.y = drag_start.y
+	else:
+		new_position.y = mouse_position.y
+	
+	return new_position
 
 func update_player_data_ui(data):
 	$"Main-ui/level".text = "Level: " + str(int(data.level))
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -4,6 +4,7 @@ var damage = 2
 var health = 10
 const speed = 100
 var current_command = "stop"
+var control = "player"
 
 var click_position = Vector2()
 var target_position = Vector2()
@@ -13,12 +14,9 @@ func _ready():
 	click_position = position
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("right_click"):
-		click_position = get_global_mouse_position()
-		current_command = "move"
-	if Input.is_action_just_pressed("stop_movement"):
-		click_position = position
-		current_command = "stop"
+	#if Input.is_action_just_pressed("stop_movement"):
+		#click_position = position
+		#current_command = "stop"
 	if position.distance_to(click_position) > 3:
 		target_position = (click_position - position).normalized()
 		velocity = target_position * speed
