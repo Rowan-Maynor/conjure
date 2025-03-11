bbae59f80b4429fa0b5dbb4d5abe2868a67cee1c
rowan-maynor <rowanmaynor@gmail.com>
2025-03-11 17:11:10 -0400

    Made entire map smaller
    big map was causing issues with resolution changes

---
+++ scripts/spawn_t1.gd
@@ -0,0 +1,23 @@
+extends Button
+
+var t1_units = [
+	"res://scenes/units/t1/pig.tscn",
+	"res://scenes/units/t1/skeleton.tscn"
+]
+
+func _on_pressed():
+	var spawn_areas = get_tree().get_root().get_node("main/player_spawn_areas").get_children()
+	var unit = load(t1_units.pick_random()).instantiate()
+	var spawn_point = find_open_spawn_point(spawn_areas)
+	if(spawn_point == null):
+		print("No free space!")
+	else:
+		unit.position = spawn_point.global_position
+		get_tree().get_root().get_node("main").add_child(unit)
+		
+func find_open_spawn_point(spawn_areas):
+	for area in spawn_areas:
+		var units = area.has_overlapping_bodies()
+		if(units == false):
+			return area
+	return null
