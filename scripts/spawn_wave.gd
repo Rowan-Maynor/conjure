b783d159d780adb293e40eee5fdf398e049995a4
rowan-maynor <rowanmaynor@gmail.com>
2025-03-11 22:09:47 -0400

    Added enemy spawn mechanics

---
+++ scripts/spawn_wave.gd
@@ -0,0 +1,14 @@
+extends Button
+
+var t1_units = [
+	"res://scenes/units/t1/pig.tscn",
+	"res://scenes/units/t1/skeleton.tscn"
+]
+
+func _on_pressed():
+	var spawn_areas = get_tree().get_root().get_node("main/enemy_spawn_areas").get_children()
+	for spawn_point in spawn_areas:
+		var unit = load(t1_units.pick_random()).instantiate()
+		unit.position = spawn_point.global_position
+		unit.control = "enemy"
+		get_tree().get_root().get_node("main").add_child(unit)
