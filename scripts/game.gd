0a94f9a6bab2c0b4e41f0dceb721c75128b4bf2a
rowan-maynor <rowanmaynor@gmail.com>
2025-05-19 23:51:16 -0400

    Add keybind to print orphans

--- scripts/game.gd
+++ scripts/game.gd
@@ -81,6 +81,8 @@ func _ready():
 	save_player_data()
 
 func _input(event: InputEvent) -> void:
+	if(Input.is_action_just_pressed("print_orphans")):
+		print_orphan_nodes()
 	if(Input.is_action_just_pressed("toggle_fullscreen")):
 		if(DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
